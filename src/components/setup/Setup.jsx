1a5c695f0e5f896f92cf5375d73df6c74faf1ba5
Connor Mann <connor@cnnrmnn.com>
2021-06-17 20:03:25 -0400

    Add calendars to setup page

--- src/components/setup/Setup.jsx
+++ src/components/setup/Setup.jsx
@@ -1,13 +1,29 @@
+import { useEffect, useState } from 'react';
 import Pane from './Pane';
+import LabeledCheckbox from '../generic/LabeledCheckbox';
+import { getCalendars } from '../../api/calendar';
 import styles from './Setup.css';
 
 export default function Setup() {
+  const [calendars, setCalendars] = useState([]);
+  useEffect(() => {
+    async function updateCalendars() {
+      setCalendars(await getCalendars());
+    }
+    updateCalendars();
+  }, []);
+
   return (
     <div className={styles.container}>
       <Pane>
         <h1 className={styles.welcome}>Welcome!</h1>
         <h3>Let's get started</h3>
-        <p>Which calendars would you like to use?</p>
+        <p className={styles.subheading}>
+          Which calendars would you like to use?
+        </p>
+        {calendars.map((calendar) => (
+          <LabeledCheckbox key={calendar.id} text={calendar.name} />
+        ))}
       </Pane>
     </div>
   );
