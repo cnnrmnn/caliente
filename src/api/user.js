987a2584be427959068f2c8cb257bf0114284a4f
Connor Mann <connor@cnnrmnn.com>
2021-06-19 17:43:13 -0400

    Add update user API helper

--- src/api/user.js
+++ src/api/user.js
@@ -1,5 +1,13 @@
 import { fetchWithDefaults } from './fetch';
 
+const USERS_ENDPOINT = `${process.env.SERVER_URL}/users/me`;
 export async function me() {
-  return fetchWithDefaults(`${process.env.SERVER_URL}/users/me`);
+  return fetchWithDefaults(USERS_ENDPOINT);
+}
+
+export async function updateUser(id, body) {
+  return fetchWithDefaults(`${USERS_ENDPOINT}`, {
+    method: 'PATCH',
+    body: body,
+  });
 }
