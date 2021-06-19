83f7dde0ed7c1ddf0aa08632b892e005ad828e2c
Connor Mann <connor@cnnrmnn.com>
2021-06-19 15:45:43 -0400

    Add dynamic value and `onBlur` event to text input

--- src/components/generic/TextInput.jsx
+++ src/components/generic/TextInput.jsx
@@ -1,12 +1,18 @@
 import styles from './TextInput.css';
 
-export default function TextInput({ value, placeholder }) {
+export default function TextInput({ value, setValue, placeholder, onBlur }) {
+  function handleChange(event) {
+    setValue(event.target.value);
+  }
+
   return (
     <input
       className={styles.input}
       type="text"
       value={value}
       placeholder={placeholder}
+      onChange={handleChange}
+      onBlur={onBlur}
     />
   );
 }
