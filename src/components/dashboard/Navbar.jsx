b16bc714e24168eae4e8fcf9fe3754cd3d99531c
Connor Mann <connor@cnnrmnn.com>
2021-06-20 15:15:20 -0400

    Add user dropdown to navbar

--- src/components/dashboard/Navbar.jsx
+++ src/components/dashboard/Navbar.jsx
@@ -1,9 +1,24 @@
+import { useContext } from 'react';
+import Dropdown from '../generic/Dropdown';
+import DropdownItem from '../generic/DropdownItem';
+import { logOut } from '../../api/user';
+import UserContext from '../../context/user';
 import styles from './Navbar.css';
 
 export default function Navbar() {
+  const { user, setUser } = useContext(UserContext);
+
+  async function handleLogOut() {
+    await logOut();
+    setUser(null);
+  }
+
   return (
     <div className={styles.navbar}>
       <h1 className={styles.logo}>caliente</h1>
+      <Dropdown title={`${user.first_name} ${user.last_name}`}>
+        <DropdownItem text="Log out" onClick={handleLogOut} />
+      </Dropdown>
     </div>
   );
 }
